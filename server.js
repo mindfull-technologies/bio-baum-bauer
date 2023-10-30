5d892a075b6cf56863aa7c39616704a344760f7e
Jamal <sayedjamal.ahmadi@dci-student>
2023-10-30 14:07:03 +0100

    Structure Set up v.2

--- server.js
+++ server.js
@@ -14,10 +14,15 @@ app.use(express.json())
 
 // routes
 app.use('/api/users', userRoute)
+// app.use('/api/product', producRoute)
 
 
 // connecting to database
-// mongoose.connect();
+mongoose.connect(`${process.env.DB_CONNECTION}`).then(() => {
+    console.log("Database connected..!");
+}).catch(error => {
+    console.log("Failed to connect :", error.message);
+})
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log("Server is listening...");
