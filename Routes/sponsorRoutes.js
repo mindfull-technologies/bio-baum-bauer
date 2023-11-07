f9461944d976718f2b8a46973cdef887cf665339
Shaqayeq-taheri <shaqayeq.taheritizro@dci.education>
2023-11-07 12:17:58 +0100

    add validation to create a new sponsor

--- Routes/sponsorRoutes.js
+++ Routes/sponsorRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { createSponsor, deleteSponsor, getAllSponsors, updateSponsor } from '../Controllers/sponsorController.js'
+import { emailValidator, nameValidator, passwordValidator } from '../Helpers/userValidation.js'
+import {validate} from '../Helpers/sponsorValidation.js'
 
 
 const router  = express.Router()
 router.get('/allSponsors', getAllSponsors)
-router.post('/newSponsor', createSponsor)
+router.post('/newSponsor', nameValidator(),emailValidator(),passwordValidator(),validate,createSponsor)
 router.delete('/deleteSponsor/:id',deleteSponsor)
 router.patch('/updateSponsor/:id',updateSponsor)
 
