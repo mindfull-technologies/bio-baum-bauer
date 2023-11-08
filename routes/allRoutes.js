d36ee18044e1a6acb232f7dce307181c85c193e2
Milica-Radulovic <milica.radulovic@dci.education>
2023-11-08 17:39:56 +0100

    add feature/Feedback-Schema-model-CRUD/Payment-Schema-model

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,20 +1,20 @@
-import express from 'express';
-import userRoutes from './userRoutes.js'
-import feedbackRoutes from './feedbackRoutes.js';
-import sponsorShipRoutes from './sponsorShipRoutes.js'
-import contactRoutes from './contactRoutes.js'
-import newsRoutes from './newsRoutes.js'
-import farmRoute from './farmRoutes.js'
-import faqRoute from './faqRoutes.js'
+import express from "express";
+import userRoutes from "./userRoutes.js";
+import feedbackRoutes from "./feedbackRoutes.js";
+import sponsorShipRoutes from "./sponsorShipRoutes.js";
+import contactRoutes from "./contactRoutes.js";
+import newsRoutes from "./newsRoutes.js";
+import farmRoute from "./farmRoutes.js";
+import faqRoute from "./faqRoutes.js";
 
 const router = express.Router();
 
-router.use('/users', userRoutes);
-router.use('/feedback', feedbackRoutes);
-router.use('/sponsorShip', sponsorShipRoutes);
-router.use('/contact', contactRoutes);
-router.use('/news', newsRoutes);
-router.use('/farm', farmRoute)
-router.use('/faq', faqRoute)
+router.use("/users", userRoutes);
+router.use("/feedback", feedbackRoutes);
+router.use("/sponsorShip", sponsorShipRoutes);
+router.use("/contact", contactRoutes);
+router.use("/news", newsRoutes);
+router.use("/farm", farmRoute);
+router.use("/faq", faqRoute);
 
 export default router;
