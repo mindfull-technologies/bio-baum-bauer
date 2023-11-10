6c487c6c2884e72e9f9828453466b4356d94e84c
Jamal <sayedjamal.ahmadi@dci-student>
2023-11-10 09:38:22 +0100

    User Validation & Sanitization Part Refactory

--- helpers/userValidation.js
+++ helpers/userValidation.js
@@ -1,58 +1,81 @@
 import { body, param, validationResult } from "express-validator";
-import { changeToUpperCase } from "./userSanitization.js";
+import { changeToUpperCase } from "./generalSanitizers.js";
 import { StatusCodes } from "http-status-codes";
 import User from "../models/User.js";
-export const nameValidator = () => body(['firstName', 'lastName'])
-    .notEmpty()
-    .withMessage('FirstName and LastName should not be empty..!')
-    .isLength({ min: 3, max: 18 })
-    .withMessage('The length of FirsName and LastName should be between 3 and 20')
-    .customSanitizer(value => changeToUpperCase(value))
-    .trim()
+import { ObjectId } from 'mongodb';
+/**
+ * Validations for the fields of user
+ */
+export const allUserFieldValidator = [
+    body(['firstName', 'lastName'])
+        .trim()
+        .notEmpty()
+        .withMessage('FirstName and LastName should not be empty..!')
+        .isLength({ min: 3, max: 18 })
+        .withMessage('The length of FirsName and LastName should be between 3 and 20')
+        .customSanitizer(value => changeToUpperCase(value)),
+    body('address.city')
+        .trim()
+        .notEmpty()
+        .withMessage('city is required'),
+    body('address.zipCode')
+        .trim()
+        .notEmpty()
+        .withMessage('zipCode is required'),
+    body('address.houseNumber')
+        .trim()
+        .notEmpty()
+        .withMessage('House Number is required!'),
+    body('email')
+        .trim()
+        .notEmpty()
+        .withMessage('Email should not be empty...!')
+        .isEmail()
+        .withMessage('This field is an email address..!')
+        .custom(async value => {
+            const isThisEmailExist = await User.findOne({ email: value });
+            if (isThisEmailExist) {
+                throw new Error(`A user with email address ${value} already exist!`)
+            }
+        }),
+    body('mobilePhone')
+        .trim()
+        .notEmpty()
+        .withMessage('Mobile Number Should not be empty..!'),
+    // .isMobilePhone('de-DE')
+    // .withMessage('Only Germany Phone Number is accepted..!'),
+    body('password')
+        .trim()
+        .notEmpty()
+        .withMessage('Password should not be empty..!')
+        .isStrongPassword()
+        .withMessage('Password needs to contain at least 8 characters, minimum one lower case character, minimum one uppercase character, minimum one number and minimum one symbol.'),
+    body('passwordConfirmation')
+        .trim()
+        .notEmpty()
+        .withMessage('Password Confirmation should not left empty..!')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error("password should match with Confirm Password");
+            }
+            return true;
+        })
+]
 
-export const cityValidator = () => body('address.city')
-    .notEmpty()
-    .withMessage('city is required')
-
-export const zipCodeValidator = () => body('address.zipCode')
-    .notEmpty()
-    .withMessage('zipCode is required')
-
-export const houseNumberValidator = () => body('address.houseNumber')
-    .notEmpty()
-    .withMessage('House Number is required!')
-
-export const emailValidator = () => body('email')
-    .notEmpty()
-    .withMessage('Email should not be empty...!')
-    .isEmail()
-    .withMessage('This field is an email address..!')
-    .custom(async value => {
-        const isThisEmailExist = await User.findOne({ email: value });
-        if (isThisEmailExist) {
-            throw new Error(`A user with email address ${value} already exist!`)
-        }
-    })
-    .trim()
-
-export const mobilePhoneValidator = () => body('mobilePhone')
-    .notEmpty()
-    .withMessage('Mobile Number Should not be empty..!')
-// .isMobilePhone('de-DE')
-// .withMessage('Only Germany Phone Number is accepted..!')
-
-export const passwordValidator = () => body('password')
-    .notEmpty()
-    .withMessage('Password should not be empty..!')
-    .isStrongPassword()
-    .withMessage('Password needs to contain at least 8 characters, minimum one lower case character, minimum one uppercase character, minimum one number and minimum one symbol.')
-
-
-
-export const parameterValidator = () => param('uId')
-    .notEmpty()
-    .withMessage("parameter should not left empty!")
-    
+/**
+ * this function is used for parameter Validation
+ * @returns 
+ */
+export const parameterValidator = [
+    param('uId')
+        .custom(value => {
+            // the parameter need to be only from ObjectId type
+            if (!ObjectId.isValid(value)) {
+                throw new Error('Parameter can be only of type ObjectId');
+            }
+            return true;
+        })
+]
 
 /**
  * for validating
@@ -61,14 +84,14 @@ export const parameterValidator = () => param('uId')
  * @param {*} next 
  * @returns 
  */
-export const validate = (req, res, next) => {
+export const validateResultUser = (req, res, next) => {
     const errors = validationResult(req);
     //if there are errors 
     if (!errors.isEmpty()) {
         //response code 400
         return res.status(StatusCodes.BAD_REQUEST).json({ errors: errors.array() })
     }
-
+    // if there is not any error, should pass the control to next middleware
     next();
 }
 
--- routes/sponsorShipRoutes.js
+++ routes/sponsorShipRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { createSponsor, deleteSponsor, getAllSponsors, updateSponsor } from '../controllers/sponsorShipController.js'
-import { emailValidator, nameValidator, passwordValidator } from '../helpers/userValidation.js'
+// import { emailValidator, nameValidator, passwordValidator } from '../helpers/userValidation.js'
 import { validate } from '../helpers/sponsorValidation.js'
 
 const router  = express.Router();
 
 router.get('/allSponsors', getAllSponsors)
-router.post('/newSponsor', nameValidator(),emailValidator(),passwordValidator(), validate, createSponsor)
+// router.post('/newSponsor', nameValidator(),emailValidator(),passwordValidator(), validate, createSponsor)
 router.delete('/deleteSponsor/:id',deleteSponsor)
 router.patch('/updateSponsor/:id',updateSponsor)
 
