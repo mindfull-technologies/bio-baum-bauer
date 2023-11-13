5a9b4ea67d3eeeeeb409d0e65a9ed053b0e72985
Shaqayeq-taheri <shaqayeq.taheritizro@dci.education>
2023-11-13 11:08:44 +0100

    validation of sponsorship modified(validationResult middleware added)

--- routes/sponsorShipRoutes.js
+++ routes/sponsorShipRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import { createSponsor, deleteSponsor, getAllSponsors, updateSponsor } from '../controllers/sponsorShipController.js'
 
-import { sponsorshipFullValidation } from '../helpers/sponsorValidation.js';
+import { sponsorshipFullValidation, validationResultSponsor } from '../helpers/sponsorValidation.js';
 
 
 const router  = express.Router();
 
 router.get('/allSponsors', getAllSponsors)
-router.post('/newSponsor',sponsorshipFullValidation ,createSponsor)
+router.post('/newSponsor',sponsorshipFullValidation,validationResultSponsor ,createSponsor)
 router.delete('/deleteSponsor/:id',deleteSponsor)
 router.patch('/updateSponsor/:id',updateSponsor)
 
