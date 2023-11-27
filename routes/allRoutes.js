3e5313b129b5324b27f11a2e180b157dcb8ebd80
Jamal <sayedjamal.ahmadi@dci-student>
2023-11-27 13:17:31 +0100

    Fixing bug

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -3,7 +3,6 @@ import userRoutes from "./userRoutes.js";
 import feedbackRoutes from "./feedbackRoutes.js";
 import sponsorShipRoutes from "./sponsorShipRoutes.js";
 import contactRoutes from "./contactRoutes.js";
-import newsRoutes from "./newsRoutes.js";
 import newsArticleRouter from "./newsArticleRoutes.js"
 import farmRoute from "./farmRoutes.js";
 import faqRoute from "./faqRoutes.js";
@@ -16,7 +15,6 @@ router.use("/users", userRoutes);
 router.use("/feedback", feedbackRoutes);
 router.use("/sponsorShip", sponsorShipRoutes);
 router.use("/contact", contactRoutes);
-router.use("/news", newsRoutes);
 // New news articles routes
 router.use("/newsArticle", newsArticleRouter);
 router.use("/farm", farmRoute);
