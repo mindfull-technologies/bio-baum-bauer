5c93fd0888291e2209e354d4ec8240a292957118
Chandu Sasidharan <mail@chandu-sasidharan.de>
2025-01-02 17:55:47 +0100

    enable external access by setting host to true in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [react()],
-})
+    server: {
+        host: true, // Allows external access by exposing to 0.0.0.0
+        port: 3000, // Ensure it listens on port 3000
+    },
+    plugins: [react()],
+});
