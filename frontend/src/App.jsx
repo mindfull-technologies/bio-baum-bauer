5f3fd16a0bad8cac8b6d1c063bd8c7db436458ff
Chandu Sasidharan <mail@chandu-sasidharan.de>
2025-01-03 14:56:04 +0100

    add import directive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,47 @@
-import { useEffect } from "react";
-import { CartContextProvider } from "./store/CartContext";
-import { AuthProvider } from "./contexts/AuthContext";
+import { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   useLocation,
-} from "react-router-dom";
+} from 'react-router-dom';
+import { CartContextProvider } from './store/CartContext';
+import { AuthProvider } from './contexts/AuthContext';
 
 // Importing our pages
-import Home from "./pages/Home";
-import Trees from "./pages/Trees";
-import Index from "./pages/Index";
-import News from "./pages/News";
-import About from "./pages/About";
-import Gallery from "./pages/Gallery";
-import Faq from "./pages/Faq";
-import Contact from "./pages/Contact";
-import Cart from "./pages/user/Cart";
-import Login from "./pages/user/Login";
-import Register from "./pages/user/Register";
-import NotFound from "./pages/NotFound";
-import Dashboard from "./pages/user/Dashboard";
-import UpdateProfile from "./pages/user/UpdateProfile";
-import UserSponsorships from "./pages/user/UserSponsorships";
-import PasswordChange from "./pages/user/PasswordChange";
-import Signout from "./pages/user/Signout";
-import SingleTreePage from "./pages/SingleTreePage";
-import NewsArticle from "./pages/NewsArticle";
-import Checkout from "./pages/user/Checkout";
-import Order from "./pages/user/Order";
+import Home from '@/pages/Home';
+import Trees from './pages/Trees';
+import Index from './pages/Index';
+import News from './pages/News';
+import About from './pages/About';
+import Gallery from './pages/Gallery';
+import Faq from './pages/Faq';
+import Contact from './pages/Contact';
+import Cart from './pages/user/Cart';
+import Login from './pages/user/Login';
+import Register from './pages/user/Register';
+import NotFound from './pages/NotFound';
+import Dashboard from './pages/user/Dashboard';
+import UpdateProfile from './pages/user/UpdateProfile';
+import UserSponsorships from './pages/user/UserSponsorships';
+import PasswordChange from './pages/user/PasswordChange';
+import Signout from './pages/user/Signout';
+import SingleTreePage from './pages/SingleTreePage';
+import NewsArticle from './pages/NewsArticle';
+import Checkout from './pages/user/Checkout';
+import Order from './pages/user/Order';
 // import AddToGallery from "./pages/AddToGallery";
 // import AddToNewArticle from "./pages/AddToNewsArticle";
 // import AddNewTree from "./pages/AddNewTree";
-import Privacy from "./pages/Privacy";
-import Terms from "./pages/TermsConditions";
-import "./assets/styles/PrevNext.css";
-import SuccessPage from "./pages/SuccessPage";
-import CancelPage from "./pages/CancelPage";
-import ProtectedRoute from "./pages/user/ProtectedRoute";
+import Privacy from './pages/Privacy';
+import Terms from './pages/TermsConditions';
+import './assets/styles/PrevNext.css';
+import SuccessPage from './pages/SuccessPage';
+import CancelPage from './pages/CancelPage';
+import ProtectedRoute from './pages/user/ProtectedRoute';
 // import Contributors from "./pages/Contributors";
-import { PatronProvider } from "./store/PatronContext";
-import SponsorShipDetails from "./pages/user/SponsorShipDetails";
+import { PatronProvider } from './store/PatronContext';
+import SponsorShipDetails from './pages/user/SponsorShipDetails';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -62,21 +62,21 @@ function App() {
             <Router>
               <ScrollToTop />
               <Routes>
-                <Route path="/" element={<Index />}>
+                <Route path='/' element={<Index />}>
                   <Route index element={<Home />} />
-                  <Route path="/trees" element={<Trees />} />
-                  <Route path="/trees/:id" element={<SingleTreePage />} />
-                  <Route path="/news" element={<News />} />
-                  <Route path="/news/:id" element={<NewsArticle />} />
-                  <Route path="/privacy" element={<Privacy />} />
-                  <Route path="/Terms" element={<Terms />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path="/gallery" element={<Gallery />} />
-                  <Route path="/faq" element={<Faq />} />
-                  <Route path="/contact" element={<Contact />} />
+                  <Route path='/trees' element={<Trees />} />
+                  <Route path='/trees/:id' element={<SingleTreePage />} />
+                  <Route path='/news' element={<News />} />
+                  <Route path='/news/:id' element={<NewsArticle />} />
+                  <Route path='/privacy' element={<Privacy />} />
+                  <Route path='/Terms' element={<Terms />} />
+                  <Route path='/about' element={<About />} />
+                  <Route path='/gallery' element={<Gallery />} />
+                  <Route path='/faq' element={<Faq />} />
+                  <Route path='/contact' element={<Contact />} />
                   {/* <Route path="/contributors" element={<Contributors />} /> */}
                   <Route
-                    path="/cart"
+                    path='/cart'
                     element={
                       <ProtectedRoute>
                         <Cart />
@@ -84,7 +84,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/checkout"
+                    path='/checkout'
                     element={
                       <ProtectedRoute>
                         <Checkout />
@@ -92,17 +92,17 @@ function App() {
                     }
                   />
                   <Route
-                    path="/order/place_order"
+                    path='/order/place_order'
                     element={
                       <ProtectedRoute>
                         <Order />
                       </ProtectedRoute>
                     }
                   />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
+                  <Route path='/login' element={<Login />} />
+                  <Route path='/register' element={<Register />} />
                   <Route
-                    path="/dashboard"
+                    path='/dashboard'
                     element={
                       <ProtectedRoute>
                         <Dashboard />
@@ -110,7 +110,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/update_profile"
+                    path='/update_profile'
                     element={
                       <ProtectedRoute>
                         <UpdateProfile />
@@ -118,7 +118,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/user_sponsorships"
+                    path='/user_sponsorships'
                     element={
                       <ProtectedRoute>
                         <UserSponsorships />
@@ -126,7 +126,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/password_change"
+                    path='/password_change'
                     element={
                       <ProtectedRoute>
                         <PasswordChange />
@@ -134,7 +134,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/signout"
+                    path='/signout'
                     element={
                       <ProtectedRoute>
                         <Signout />
@@ -145,7 +145,7 @@ function App() {
                 <Route path="/addToNewArticle" element={<AddToNewArticle />} />
                 <Route path="/addNewTree" element={<AddNewTree />} /> */}
                   <Route
-                    path="/success"
+                    path='/success'
                     element={
                       <ProtectedRoute>
                         <SuccessPage />
@@ -153,7 +153,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/cancel"
+                    path='/cancel'
                     element={
                       <ProtectedRoute>
                         <CancelPage />
@@ -161,14 +161,14 @@ function App() {
                     }
                   />
                   <Route
-                    path="/sponsorship-details/:id"
+                    path='/sponsorship-details/:id'
                     element={
                       <ProtectedRoute>
                         <SponsorShipDetails />
                       </ProtectedRoute>
                     }
                   />
-                  <Route path="*" element={<NotFound />} />
+                  <Route path='*' element={<NotFound />} />
                 </Route>
               </Routes>
             </Router>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        host: true, // Allows external access by exposing to 0.0.0.0
-        port: 3000, // Ensure it listens on port 3000
+  server: {
+    host: true, // Allows external access by exposing to 0.0.0.0
+    port: 3000, // Ensure it listens on port 3000
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    plugins: [react()],
+  },
 });
